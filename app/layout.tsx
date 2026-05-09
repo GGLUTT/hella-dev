79626d551ce3f5d07e788f2c716970e210d56d0e
luter <newgglutt@gmail.com>
2026-05-09 16:22:38 +0300

    fix: use favicon.png as site icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,13 +82,10 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.png", type: "image/png" },
     ],
-    apple: "/apple-touch-icon.png",
+    apple: "/favicon.png",
   },
-  manifest: "/site.webmanifest",
   verification: {
     google: "",
   },
